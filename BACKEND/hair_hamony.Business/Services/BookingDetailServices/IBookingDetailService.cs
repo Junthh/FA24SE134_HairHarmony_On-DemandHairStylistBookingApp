8b0d120df3cb0a592f63c84c4fc5d3c61ce1bc6e
trung-code <Thaivantrungcode@gmail.com>
2024-10-11 09:53:03 +0700

    booking detail service

---
+++ BACKEND/hair_hamony.Business/Services/BookingDetailServices/IBookingDetailService.cs
@@ -0,0 +1,15 @@
+﻿using hair_hamony.Business.Commons.Paging;
+using hair_hamony.Business.Enum;
+using hair_hamony.Business.ViewModels.BookingDetails;
+
+namespace hair_hamony.Business.Services.BookingDetailServices
+{
+    public interface IBookingDetailService
+    {
+        Task<(IList<GetBookingDetailModel>, int)> GetAll(PagingParam<BookingDetailEnum.BookingDetailSort> paginationModel, SearchBookingDetailModel searchBookingDetailModel);
+        Task<GetBookingDetailModel> GetById(Guid id);
+        Task<GetBookingDetailModel> Create(CreateBookingDetailModel requestBody);
+        Task<GetBookingDetailModel> Update(Guid id, UpdateBookingDetailModel requestBody);
+        Task Delete(Guid id);
+    }
+}
---
+++ BACKEND/hair_hamony.Business/ViewModels/BookingDetails/CreateBookingDetailModel.cs
@@ -0,0 +1,11 @@
+﻿namespace hair_hamony.Business.ViewModels.BookingDetails
+{
+    public class CreateBookingDetailModel
+    {
+        public double? Price { get; set; }
+        public int? Duration { get; set; }
+        public Guid? BookingId { get; set; }
+        public Guid? ServiceId { get; set; }
+        public Guid? ComboId { get; set; }
+    }
+}
