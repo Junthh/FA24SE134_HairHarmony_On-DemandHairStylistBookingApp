fd751f405b5789311c7a3aa9d8170a3679a720bc
trung-code <Thaivantrungcode@gmail.com>
2024-10-19 15:17:17 +0700

    workship service

---
+++ BACKEND/hair_hamony.Business/ViewModels/Workships/SearchWorkshipModel.cs
@@ -0,0 +1,9 @@
+﻿namespace hair_hamony.Business.ViewModels.Workships
+{
+    public class SearchWorkshipModel
+    {
+        public TimeOnly? StartTime { get; set; }
+        public TimeOnly? EndTime { get; set; }
+        public DateTime? CreatedDate { get; set; }
+    }
+}
