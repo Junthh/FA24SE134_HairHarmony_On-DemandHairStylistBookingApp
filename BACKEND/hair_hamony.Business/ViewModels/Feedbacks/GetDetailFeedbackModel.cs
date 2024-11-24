1d7f9570ffbfb41d5feb6cf76d90dc82f89e6dac
trung-code <Thaivantrungcode@gmail.com>
2024-11-24 22:35:21 +0700

    update api get all feedback

---
+++ BACKEND/hair_hamony.Business/ViewModels/Feedbacks/GetDetailFeedbackModel.cs
@@ -0,0 +1,15 @@
+﻿using hair_hamony.Business.ViewModels.Bookings;
+
+namespace hair_hamony.Business.ViewModels.Feedbacks
+{
+    public class GetDetailFeedbackModel
+    {
+        public Guid Id { get; set; }
+        public double? Rating { get; set; }
+        public string? Description { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public Guid? BookingId { get; set; }
+        public Guid? StylistId { get; set; }
+        public GetDetailBookingModel? Booking { get; set; }
+    }
+}
