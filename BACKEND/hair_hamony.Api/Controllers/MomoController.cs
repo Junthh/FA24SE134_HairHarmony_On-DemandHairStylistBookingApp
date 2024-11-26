97bcdb0c93ae9f7e0e84d754374f2c11229d9c7a
trung-code <Thaivantrungcode@gmail.com>
2024-11-26 22:27:04 +0700

    api create url momo payment

---
+++ BACKEND/hair_hamony.Api/Controllers/MomoController.cs
@@ -0,0 +1,32 @@
+﻿using hair_hamony.Business.Services.MomoServices;
+using hair_hamony.Business.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hair_hamony.API.Controllers
+{
+    public class MomoController : BaseApiController
+    {
+        private readonly IMomoService _momoService;
+        public MomoController(IMomoService momoService)
+        {
+            _momoService = momoService;
+        }
+
+        /// <summary>
+        /// Endpoint for create url momo payment
+        /// </summary>
+        /// <returns>Url momo payment</returns>
+        /// <response code="200">Returns a url momo payment</response>
+        [HttpPost()]
+        [AllowAnonymous]
+        [ProducesResponseType(typeof(BaseResponse<string>), StatusCodes.Status200OK)]
+        [Produces("application/json")]
+        public async Task<IActionResult> Create()
+        {
+            return Ok(new BaseResponse<string>(
+                data: await _momoService.Create()
+            ));
+        }
+    }
+}
