579fd836250b49c5c37bf55f23d33c47fa720825
trung-code <Thaivantrungcode@gmail.com>
2024-12-07 11:17:39 +0700

    api get total booking by status

---
+++ BACKEND/hair_hamony.Business/ViewModels/Bookings/GetBookingByStatusModel.cs
@@ -0,0 +1,12 @@
+﻿namespace hair_hamony.Business.ViewModels.Bookings
+{
+    public class GetBookingByStatusModel
+    {
+        public int Initialize { get; set; }
+        public int Confirmed { get; set; }
+        public int Processing { get; set; }
+        public int Completed { get; set; }
+        public int Finished { get; set; }
+        public int Cancel { get; set; }
+    }
+}
