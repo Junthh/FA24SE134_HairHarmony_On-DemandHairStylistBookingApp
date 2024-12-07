fa1b522aa8983f7c32a85c2189a73212293176bf
lucnm <lucnm007@gmail.com>
2024-12-07 12:24:06 +0700

    update dashboard owner

--- FRONTEND/Owner/src/pages/Owner/Dashboard/index.tsx
+++ FRONTEND/Owner/src/pages/Owner/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 import { Avatar, Box, Divider, Grid, Typography } from '@mui/material';
 import * as colors from 'constants/colors';
@@ -7,7 +7,20 @@ import { DateCalendar, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import ReactApexChart from 'react-apexcharts';
 import { ApexOptions } from 'apexcharts';
-
+import moment from 'moment';
+import { dashboardServices } from 'services/dashboard.service';
+import { useDispatch } from 'react-redux';
+import { setLoading } from 'redux/Reducer';
+import {
+  OptionsAreaConfig,
+  OptionsBarConfig,
+  OptionsLineColumnsConfig,
+  OptionsPieConfig,
+} from './OptionsChart';
+import { currencyFormat } from 'utils/helper';
+import { MAP_STATUS_BOOKING } from 'constants/status';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import CollectionsBookmarkIcon from '@mui/icons-material/CollectionsBookmark';
 const TimekeepingStyled = styled(Box)({
   padding: '10px 20px',
   '& .card-total': {
@@ -35,205 +48,110 @@ const TimekeepingStyled = styled(Box)({
   },
 });
 export default function Dashboard() {
-  const [series, setSeries] = useState([
-    {
-      name: 'Net Profit',
-      data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
-    },
-    // {
-    //   name: 'Revenue',
-    //   data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
-    // },
-    // {
-    //   name: 'Free Cash Flow',
-    //   data: [35, 41, 36, 26, 45, 48, 52, 53, 41],
-    // },
-  ]);
+  const dispatch = useDispatch();
+  const [boxSummary, setBoxSummary] = useState({
+    totalRevenue: 0,
+    totalNumberBooking: 0,
+    totalCustomer: 0,
+  });
+  const [optionsBar, setOptionsBar] = useState(OptionsBarConfig);
+  const [optionsPie, setOptionsPie] = useState(OptionsPieConfig);
+  const [optionsLineColumns, setOptionsLineColumns] = useState(OptionsLineColumnsConfig);
+  const [optionsArea, setOptionsArea] = useState(OptionsAreaConfig);
+  const handleGetDashboardData = async () => {
+    const params = {
+      month: String(Number(moment().get('M').toString()) + 1),
+      year: moment().get('years').toString(),
+    };
 
-  const options: ApexOptions = {
-    chart: {
-      type: 'bar',
-      height: 400,
-    },
-    plotOptions: {
-      bar: {
-        horizontal: false,
-        columnWidth: '55%',
-        borderRadius: 5,
-        borderRadiusApplication: 'end',
-      },
-    },
-    dataLabels: {
-      enabled: false,
-    },
-    stroke: {
-      show: true,
-      width: 2,
-      colors: ['transparent'],
-    },
-    xaxis: {
-      categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
-    },
-    yaxis: {
-      title: {
-        text: '$ (thousands)',
-      },
-    },
-    fill: {
-      opacity: 1,
-    },
-    tooltip: {
-      y: {
-        formatter: function (val) {
-          return '$ ' + val + ' thousands';
-        },
-      },
-    },
-  };
-  const optionsPie: ApexOptions = {
-    series: [44, 55, 13, 43, 22],
-    chart: {
-      width: 480,
-      type: 'pie',
-    },
-    labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
-    responsive: [
-      {
-        breakpoint: 480,
-        options: {
-          chart: {
-            width: 200,
+    try {
+      dispatch(setLoading(true));
+
+      // Fetch data in parallel
+      const [revenueRes, countRes, countCusRes] = await Promise.all([
+        dashboardServices.listTotalRevenueByMonth(params),
+        dashboardServices.listCountByStatus(params),
+        dashboardServices.listCountCustomer(),
+      ]);
+
+      // Handle Total Revenue Data
+      if (revenueRes?.data) {
+        const xAxisCategories = revenueRes.data.totalRevenueByDay.map((item) => item.day);
+        const seriesData = revenueRes.data.totalRevenueByDay.map((item) => item.totalRevenue);
+        setBoxSummary((prev) => ({ ...prev, totalRevenue: revenueRes.data.totalRevenue }));
+        setOptionsBar((prev) => ({
+          ...prev,
+          series: [
+            {
+              name: 'Tổng doanh thu',
+              data: seriesData,
+            },
+          ],
+          xaxis: {
+            ...prev.xaxis,
+            categories: xAxisCategories,
           },
-          legend: {
-            position: 'bottom',
-          },
-        },
-      },
-    ],
+        }));
+      }
+
+      // Handle Booking Count Data
+      if (countRes?.data) {
+        const totalBooking: number = Object.values(countRes.data).reduce(
+          (sum: number, value: number) => sum + value,
+          0,
+        ) as number;
+        const seriesData = Object.values(countRes.data).map((item) => item) as [];
+        const labelData = Object.keys(countRes.data).map(
+          (item: string) => MAP_STATUS_BOOKING[item].label,
+        ) as [];
+        setBoxSummary((prev) => ({ ...prev, totalNumberBooking: totalBooking }));
+        setOptionsPie((prev) => ({
+          ...prev,
+          series: seriesData,
+          labels: labelData,
+        }));
+      }
+      if (countCusRes?.data) {
+        setBoxSummary((prev) => ({ ...prev, totalCustomer: (countCusRes as any)?.paging.total }));
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      dispatch(setLoading(false));
+    }
   };
-  const optionsLineColumns: ApexOptions = {
-    series: [
-      {
-        name: 'Website Blog',
-        type: 'column',
-        data: [440, 505, 414, 671, 227, 413, 201, 352, 752, 320, 257, 160],
-      },
-      {
-        name: 'Social Media',
-        type: 'line',
-        data: [23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16],
-      },
-    ],
-    chart: {
-      height: 400,
-      type: 'line',
-    },
-    stroke: {
-      width: [0, 4],
-    },
-    title: {
-      text: 'Traffic Sources',
-    },
-    dataLabels: {
-      enabled: true,
-      enabledOnSeries: [1],
-    },
-    labels: [
-      '01 Jan 2001',
-      '02 Jan 2001',
-      '03 Jan 2001',
-      '04 Jan 2001',
-      '05 Jan 2001',
-      '06 Jan 2001',
-      '07 Jan 2001',
-      '08 Jan 2001',
-      '09 Jan 2001',
-      '10 Jan 2001',
-      '11 Jan 2001',
-      '12 Jan 2001',
-    ],
-    yaxis: [
-      {
-        title: {
-          text: 'Website Blog',
-        },
-      },
-      {
-        opposite: true,
-        title: {
-          text: 'Social Media',
-        },
-      },
-    ],
-  };
-  const optionsArea: ApexOptions = {
-    series: [
-      {
-        name: 'series1',
-        data: [31, 40, 28, 51, 42, 109, 100],
-      },
-      {
-        name: 'series2',
-        data: [11, 32, 45, 32, 34, 52, 41],
-      },
-    ],
-    chart: {
-      height: 350,
-      type: 'area',
-    },
-    dataLabels: {
-      enabled: true,
-    },
-    stroke: {
-      curve: 'smooth',
-    },
-    xaxis: {
-      type: 'datetime',
-      categories: [
-        '2018-09-19T00:00:00.000Z',
-        '2018-09-19T01:30:00.000Z',
-        '2018-09-19T02:30:00.000Z',
-        '2018-09-19T03:30:00.000Z',
-        '2018-09-19T04:30:00.000Z',
-        '2018-09-19T05:30:00.000Z',
-        '2018-09-19T06:30:00.000Z',
-      ],
-    },
-    legend: {
-      position: 'bottom', // Moves the legend to the bottom
-      horizontalAlign: 'center', // Aligns the legend in the center horizontally
-    },
-    tooltip: {
-      x: {
-        format: 'dd/MM/yy HH:mm',
-      },
-    },
-  };
+
+  useEffect(() => {
+    handleGetDashboardData();
+  }, []);
+
   return (
     <TimekeepingStyled>
       <Box className="card-total">
         <Box className="card-total_content">
           <Avatar sx={{ background: '#285d9a48', width: 80, height: 80 }}>
-            <ICONS.IconStylistUser />
+            <AttachMoneyIcon />
           </Avatar>
           <Box>
             <Typography variant="h1" fontWeight={700} fontFamily={'Lato !important'}>
-              1,234
+              {currencyFormat(boxSummary.totalRevenue)}
             </Typography>
             <Typography variant="label1" fontWeight={600} color={colors.grey2}>
-              Total Booking
+              Tổng doanh thu
             </Typography>
           </Box>
         </Box>
         <Divider orientation="vertical" variant="middle" flexItem></Divider>
         <Box className="card-total_content">
-          <Avatar sx={{ width: 80, height: 80 }}>{/* <ICONS.IconCombo /> */}</Avatar>
+          <Avatar sx={{ width: 80, height: 80 }}>
+            <CollectionsBookmarkIcon />
+          </Avatar>
           <Box>
             <Typography variant="h1" fontWeight={700} fontFamily={'Lato !important'}>
-              300
+              {boxSummary.totalNumberBooking}
             </Typography>
             <Typography variant="label1" fontWeight={600} color={colors.grey2}>
-              Số lượt dùng combo
+              Tổng lượt booking
             </Typography>
           </Box>
         </Box>
@@ -242,22 +160,27 @@ export default function Dashboard() {
           <Avatar sx={{ width: 80, height: 80 }}>{/* <ICONS.IconCalendar /> */}</Avatar>
           <Box>
             <Typography variant="h1" fontWeight={700} fontFamily={'Lato !important'}>
-              45
+              {boxSummary.totalCustomer}
             </Typography>
             <Typography variant="label1" fontWeight={600} color={colors.grey2}>
-              Số lượt dùng service
+              Số lượt khách hàng
             </Typography>
           </Box>
         </Box>
       </Box>
       <Box height={20}></Box>
       <Grid container spacing={2}>
-        <Grid item xs={6}>
+        <Grid item xs={8}>
           <Box className="card-analyst">
-            <ReactApexChart options={options} series={series} type="bar" height={350} />
+            <ReactApexChart
+              options={optionsBar}
+              series={optionsBar.series}
+              type="bar"
+              height={350}
+            />
           </Box>
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={4}>
           <Box className="card-analyst">
             <ReactApexChart
               options={optionsPie}
@@ -267,7 +190,7 @@ export default function Dashboard() {
             />
           </Box>
         </Grid>
-        <Grid item xs={6}>
+        {/* <Grid item xs={6}>
           <Box className="card-analyst">
             <ReactApexChart
               options={optionsLineColumns}
@@ -286,7 +209,7 @@ export default function Dashboard() {
               height={optionsArea.chart.height}
             />
           </Box>
-        </Grid>
+        </Grid> */}
       </Grid>
     </TimekeepingStyled>
   );
