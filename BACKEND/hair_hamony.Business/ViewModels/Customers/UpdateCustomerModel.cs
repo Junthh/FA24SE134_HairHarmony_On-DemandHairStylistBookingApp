11054977d036daa1423f818fd9e17ba2d7a1acc5
Tamuella <nguyenminhtammt2000@gmail.com>
2024-12-12 21:41:22 +0700

    hotfix update customer

--- BACKEND/hair_hamony.Business/ViewModels/Customers/UpdateCustomerModel.cs
+++ BACKEND/hair_hamony.Business/ViewModels/Customers/UpdateCustomerModel.cs
@@ -7,7 +7,6 @@ namespace hair_hamony.Business.ViewModels.Customers
         public Guid Id { get; set; }
         public int? LoyaltyPoints { get; set; }
         public string? FullName { get; set; }
-        public string? Password { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Status { get; set; }
         public IFormFile? Avatar { get; set; }
