14983abc82f7c3ba6cb3210d30ee01097fdf9e28
Koi1420 <khoikad@gmail.com>
2025-01-05 16:41:30 +0700

    update api get stylist free time

--- BACKEND/hair_hamony.Business/Services/BookingSlotStylistServices/BookingSlotStylistService.cs
+++ BACKEND/hair_hamony.Business/Services/BookingSlotStylistServices/BookingSlotStylistService.cs
@@ -123,6 +123,7 @@ namespace hair_hamony.Business.Services.BookingSlotStylistServices
                     stylistWorkship.RegisterDate.Equals(bookingDate)
                     && stylistWorkship.Workship!.StartTime <= timeSlot!.StartTime
                     && stylistWorkship.Workship!.EndTime > timeSlot!.StartTime
+                    && stylistWorkship.DayOffs.Any(x => x.IsApprove == true)
                 )
                 .Select(stylistWorkship => stylistWorkship.StylistId)
                 .ToListAsync();
